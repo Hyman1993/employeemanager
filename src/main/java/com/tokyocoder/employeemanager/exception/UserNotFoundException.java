9bdbf1dc2ee15457880c81920b3a9afc82c3461b
Hyman1993 <1392841128@qq.com>
2021-07-20 22:34:15 +0900

    implement for repo and services class

---
+++ src/main/java/com/tokyocoder/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.tokyocoder.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/tokyocoder/employeemanager/service/EmployeeService.java
@@ -0,0 +1,44 @@
+package com.tokyocoder.employeemanager.service;
+
+import com.tokyocoder.employeemanager.exception.UserNotFoundException;
+import com.tokyocoder.employeemanager.model.Employee;
+import com.tokyocoder.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmpployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAlEmpoyees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id).orElseThrow(
+                () -> new UserNotFoundException("User by id " + id + " was not found")
+        );
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+
+}
